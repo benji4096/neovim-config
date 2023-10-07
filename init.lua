b645a4e29f369f6a5d29f14b12ae7879baca5575
benji4096 <benjib4096@gmail.com>
2023-10-06 21:30:52 -0700

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+
+vim.opt.relativenumber = true -- show relative line numbers
+vim.opt.number = true -- show line number
+vim.opt.wrap = false -- line wrap
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+require("benji.lazy")
+
