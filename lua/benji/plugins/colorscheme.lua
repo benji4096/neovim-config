cd651faf3a7a28285d22aa496540555e28d7f33e
benji4096 <benjib4096@gmail.com>
2023-11-16 10:33:43 -0800

    change colorscheme

--- lua/benji/plugins/colorscheme.lua
+++ lua/benji/plugins/colorscheme.lua
@@ -1,26 +1,26 @@
 
---return {
---	"catppuccin/nvim",
---	name = "catppuccin",
---	priority = 1000,
---
---	config = function()
---		require("catppuccin").setup({
---			flavour = "mocha",
---			transparent_background = false,
---		})
---		vim.cmd.colorscheme "catppuccin"
---	end,
---}
-
 return {
-	"Mofiqul/vscode.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+
 	config = function()
-		--vim.o.background = "dark"
-		require("vscode").setup({
-			transperant = false,
+		require("catppuccin").setup({
+			flavour = "mocha",
+			transparent_background = false,
 		})
-		require("vscode").load()
-	end
+		vim.cmd.colorscheme "catppuccin"
+	end,
 }
+
+--return {
+--	"Mofiqul/vscode.nvim",
+--	config = function()
+--		--vim.o.background = "dark"
+--		require("vscode").setup({
+--			transperant = false,
+--		})
+--		require("vscode").load()
+--	end
+--}
 
