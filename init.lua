bba1dde628f60b3c3e884290ec21eb36cd291677
benji4096 <benjib4096@gmail.com>
2023-11-20 14:51:07 -0800

    alt+# to set tab

--- init.lua
+++ init.lua
@@ -5,5 +5,16 @@ vim.opt.wrap = false -- line wrap
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
+vim.keymap.set("n", "<M-1>", "1gt")
+vim.keymap.set("n", "<M-2>", "2gt")
+vim.keymap.set("n", "<M-3>", "3gt")
+vim.keymap.set("n", "<M-4>", "4gt")
+vim.keymap.set("n", "<M-5>", "5gt")
+vim.keymap.set("n", "<M-6>", "6gt")
+vim.keymap.set("n", "<M-7>", "7gt")
+vim.keymap.set("n", "<M-8>", "8gt")
+vim.keymap.set("n", "<M-9>", "9gt")
+vim.keymap.set("n", "<M-0>", "0gt")
+
 require("benji.lazy")
 
